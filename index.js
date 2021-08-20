fa2dbd338aae205832b7ce63fd3d42ec1f109eed
Ivan <ivnvaldz@gmail.com>
2021-08-19 22:23:27 -0300

    Lista para Github

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const icon = document.getElementById('icon')
-const nav = document.getElementById('nav-ul')
+const icon = document.getElementById("icon");
+const nav = document.getElementById("nav-ul");
 
-icon.addEventListener('click',()=>{
-    nav.classList.toggle('nav_ul--show')
-})
+icon.addEventListener("click", () => {
+  nav.classList.toggle("nav_ul--show");
+});
+
+const SR = ScrollReveal({
+  origin: "top",
+  duration: 2000,
+  reset: true,
+});
+SR.reveal(
+  ".principals, .principalsTwo, .principalsThree, .principalsFour, .principalContact, .seconds, .secondsTwo, .secondsThree, .third, .thirdTwo, .thirdThree",
+  {
+    interval: 200,
+  }
+);
